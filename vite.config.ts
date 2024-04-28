32f7493aedf3a53e03e3eb9fd1de9d1f53177c62
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-04-28 14:53:00 -0300

    +Añadí librerias + configure paths a ciertos archivos.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@lib': '/src/lib/modul.ts',
+      '@controlers': '/src/shared/imports/controlers.ts',
+      '@validate': '/src/shared/imports/validate.ts'
+    }
+  }
 })
