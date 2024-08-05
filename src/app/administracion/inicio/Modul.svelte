52d8ec5b157d7820af2e8a2514adbdc47c9a6157
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-08-05 20:23:23 -0300

    Cambio en inicio

--- src/app/administracion/inicio/Modul.svelte
+++ src/app/administracion/inicio/Modul.svelte
@@ -21,7 +21,7 @@
     <h1 class="text-[32px] font-[700] mb-[20px]">Bienvenido de vuelta {$loginStore.userName}!</h1>
     <h2 class="text-[24px] font-[500]">Un resumen de <span class="font-[700] text-[var(--dark-blue)]">{actualMonth}</span></h2>
     <div class="flex gap-[15px] mt-[20px]">
-        <div class="w-[30%] flex flex-col gap-y-[20px]">
+        <div class="w-[30%] flex flex-col gap-y-[10px]">
             <BillsCard month={actualMonth} url="/Administracion/Gastos" title="Ver gastos" />
             <NewSale url="/Administracion/RegistrarVenta" title="Crear nueva venta" />
         </div>
