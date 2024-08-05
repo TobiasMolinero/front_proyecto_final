67abd5886433a054a17196d97e666f1147f48e55
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-08-05 20:25:21 -0300

    Cambios en modulo crear venta

--- src/app/administracion/crear-venta/Modul.svelte
+++ src/app/administracion/crear-venta/Modul.svelte
@@ -6,6 +6,6 @@
 </script>
 
 <div class="flex flex-col h-[100%]">
-    <button on:click={() => push('/Administracion/Ventas')} title="Volver a ventas" class="text-[14px] mx-[20px] pt-[10px] place-self-start underline cursor-pointer hover:text-slate-950">Volver a ventas</button>
+    <button on:click={() => push('/Administracion/Ventas')} title="Volver a ventas" class="text-[14px] mx-[20px] pt-[10px] place-self-start underline cursor-pointer hover:text-[var(--regular-blue)]">Volver a ventas</button>
     <h2 class="font-[700] text-[24px] text-start px-[20px]">Nueva Venta</h2>
 </div>
